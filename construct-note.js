4c3bf6a6917c3bdfb90ee2512c0103ef30c82b82
Julia Ayres <j.bobrineva@gmail.com>
2021-04-30 16:30:34 -0400

    passed tests for construct-note.js

---
+++ construct-note.js
@@ -0,0 +1,22 @@
+// add whatever parameters you deem necessary
+
+function freqCounter (str) {
+	let frequency = {};
+	for (let el of str) {
+		frequency[el] = frequency[el] + 1 || 1;
+	}
+	return frequency;
+}
+
+function constructNote (msg, letters) {
+	if (msg.length > letters.length) return false;
+	const msgCounter = freqCounter(msg);
+	const lettersCounter = freqCounter(letters);
+	for (let char in msgCounter) {
+		if (!lettersCounter[char]) return false;
+		if (msgCounter[char] > lettersCounter[char]) return false;
+	}
+	return true;
+}
+
+module.exports = { freqCounter, constructNote };
