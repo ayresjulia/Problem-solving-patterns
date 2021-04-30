a18bae5b341a81a66117aa24f200ae922151cc7f
Julia Ayres <j.bobrineva@gmail.com>
2021-04-30 19:09:45 -0400

    passed tests for separate-positive.js

--- separate-positive.js
+++ separate-positive.js
@@ -2,8 +2,12 @@ function separatePositive (arr, left = 0, right = arr.length - 1) {
 	while (left < right) {
 		if (arr[left] < 0 && arr[right] > 0) {
 			[ arr[left], arr[right] ] = [ arr[right], arr[left] ];
-		} else if (arr[left] > 0 && arr[right] > 0) {
 			left++;
+			right--;
+		} else if (arr[left] > 0) {
+			left++;
+		} else {
+			right--;
 		}
 	}
 	return arr;
