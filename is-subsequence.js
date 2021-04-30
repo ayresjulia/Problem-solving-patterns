dbe91964fd0a97f694781bfb697f24d561b2429e
Julia Ayres <j.bobrineva@gmail.com>
2021-04-30 18:21:40 -0400

    passed tests for is-subsequence.js

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,15 @@
-// add whatever parameters you deem necessary
-function isSubsequence() {
+function isSubsequence (str1, str2, i = 0, j = 0) {
+	while (i < str1.length) {
+		if (j === str2.length) {
+			return false;
+		}
+		if (str1[i] === str2[j]) {
+			i++;
+		} else {
+			j++;
+		}
+	}
+	return true;
 }
+
+module.exports = { isSubsequence };
--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,17 @@
-// add whatever parameters you deem necessary
-function separatePositive() {
+function separatePositive (arr, left = 0, right = arr.length - 1) {
+	while (left < right) {
+		if (arr[left] < 0 && arr[right] > 0) {
+			[ arr[left], arr[right] ] = [ arr[right], arr[left] ];
+		} else if (arr[left] > 0 && arr[right] > 0) {
+			left++;
+		}
+	}
+	return arr;
 }
+
+// module.exports = { separatePositive };
+
+console.log(separatePositive([ 2, -1, -3, 6, -8, 10 ])); // [2, 10, 6, -3, -1, -8]
+console.log(separatePositive([ 5, 10, -15, 20, 25 ])); // [5, 10, 25, 20, -15]
+console.log(separatePositive([ -5, 5 ])); // [5, -5]
+console.log(separatePositive([ 1, 2, 3 ])); // [1, 2, 3]
