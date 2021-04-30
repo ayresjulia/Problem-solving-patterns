572e8ce81c6b5cf95ff6d711ae4b0e670439e644
Julia Ayres <j.bobrineva@gmail.com>
2021-04-30 17:19:19 -0400

    passed tests for two-array-object.js

--- average-pair.js
+++ average-pair.js
@@ -14,8 +14,3 @@ function averagePair (arr, target, left = 0, right = arr.length - 1) {
 }
 
 module.exports = { averagePair };
-
-// console.log(averagePair([ 1, 2, 3 ], 2.5)); // true
-// console.log(averagePair([ 1, 3, 3, 5, 6, 7, 10, 12, 19 ], 8)); // true
-// console.log(averagePair([ -1, 0, 3, 4, 5, 6 ], 4.1)); // false
-// console.log(averagePair([], 4)); // false
--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,22 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function makefreqCounter (arr) {
+	let res = Object.assign({}, arr);
+	return res;
 }
+
+function twoArrayObject (arr1, arr2) {
+	let obj = {};
+	const arr1Freq = makefreqCounter(arr1);
+	const arr2Freq = makefreqCounter(arr2);
+
+	for (let el in arr1Freq) {
+		if (arr2Freq[el] !== undefined) {
+			obj[arr1Freq[el]] = arr2Freq[el];
+		} else {
+			obj[arr1Freq[el]] = null;
+		}
+	}
+	return obj;
+}
+
+module.exports = { twoArrayObject };
