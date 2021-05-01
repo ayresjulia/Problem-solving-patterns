a8dac29ad2f445875c0237629f073a0a97908cfa
Julia Ayres <j.bobrineva@gmail.com>
2021-04-30 20:00:58 -0400

    passed tests for longest-fall.js

--- longest-fall.js
+++ longest-fall.js
@@ -1,3 +1,15 @@
-// add whatever parameters you deem necessary
-function longestFall() {
+function longestFall (arr, count = 1, maxCount = 0) {
+	if (arr.length === 0) return 0;
+
+	for (let i = 1; i < arr.length; i++) {
+		if (arr[i] < arr[i - 1]) {
+			count++;
+			maxCount = Math.max(count, maxCount);
+		} else {
+			count = 1;
+		}
+	}
+	return maxCount || 1;
 }
+
+module.exports = { longestFall };
