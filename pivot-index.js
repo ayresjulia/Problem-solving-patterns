705d158d181b3e041df2916c9e26050fd682421a
Julia Ayres <j.bobrineva@gmail.com>
2021-04-30 20:12:42 -0400

    passed tests for pivot-index.js

--- pivot-index.js
+++ pivot-index.js
@@ -1,3 +1,15 @@
-// add whatever parameters you deem necessary
-function pivotIndex() {
+function pivotIndex (arr, left = 0) {
+	let pivot;
+	let totalSum = arr.reduce((a, b) => a + b, 0);
+	for (let i = 0; i < arr.length; i++) {
+		left += arr[i];
+		if (left === totalSum) {
+			pivot = i;
+			break;
+		}
+		totalSum -= arr[i];
+	}
+	return pivot || -1;
 }
+
+module.exports = { pivotIndex };
